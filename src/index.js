0a7993a833c76a534bf7cdd0480a498f58f6f8b2
YashVar304 <yash21153028@akgec.ac.in>
2024-07-29 17:46:48 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import connectDB from "./db/index.js";
 import dotenv from 'dotenv'
+import app from "./app.js";
 
 dotenv.config({
     path:'./env'
 })
 
-connectDB()
+connectDB().then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`App is running on port ${process.env.PORT || 8000}`)
+    })
+}).catch((err)=>console.log(err))
 
 
 
---
+++ src/utils/apiError.js
@@ -0,0 +1,21 @@
+class ApiError extends Error {
+    constructor(
+        statuscode,
+        message="Something went wrong",
+        error=[],
+        stack="",
+    ){
+        super(message)
+        this.statuscode = statuscode
+        this.message = message
+        this.success = false
+        this.error = error
+        if(stack){
+            this.stack = stack
+        }else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+
+export {ApiError}
